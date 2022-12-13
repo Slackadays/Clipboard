a176481a534376e48c836bcec7c8d413ee8d7758
Slackadays <lightingdzeyenr@gmail.com>
2022-12-13 16:08:06 -0500

    Add cut tests

--- tests/test_files.sh
+++ tests/test_files.sh
@@ -1,15 +1,18 @@
 #setup
-echo "Blah!" > dummy.txt
+echo "Foobar" > copyme.txt
 mkdir dummydir
-echo "Blah!" > dummydir/foo.txt
+echo "Foobar" > dummydir/copyme.txt
+echo "Foobar" > cutme.txt
+mkdir cutdummydir
+echo "Foobar" > dummydir/cutme.txt
 
 #test copying a file and a directory
-clipboard copy dummy.txt dummydir
-if [ ! -f "$TMPDIR"/Clipboard/0/dummy.txt ]; then
+clipboard copy copyme.txt dummydir
+if [ ! -f "$TMPDIR"/Clipboard/0/copyme.txt ]; then
 echo did not copy file
   exit 1
 fi
-if [ ! -f "$TMPDIR"/Clipboard/0/dummydir/foo.txt ]; then
+if [ ! -f "$TMPDIR"/Clipboard/0/dummydir/copyme.txt ]; then
   echo did not copy directory
   exit 1
 fi
@@ -18,11 +21,35 @@ fi
 mkdir dummydir2
 cd dummydir2
 clipboard paste
-if [ ! -f dummy.txt ]; then
+if [ ! -f copyme.txt ]; then
   echo did not paste file
   exit 1
 fi
-if [ ! -f dummydir/foo.txt ]; then
+if [ ! -f dummydir/copyme.txt ]; then
+  echo did not paste file in directory
+  exit 1
+fi
+
+#test cutting a file and a directory
+cd ..
+clipboard cut cutme.txt cutdummydir
+if [ ! -f "$TMPDIR"/Clipboard/0/cutme.txt ]; then
+  echo did not cut file
+  exit 1
+fi
+if [ ! -f "$TMPDIR"/Clipboard/0/cutdummydir/cutme.txt ]; then
+  echo did not cut directory
+  exit 1
+fi
+
+#test pasting a file and a directory that were cut
+cd dummydir2
+clipboard paste
+if [ ! -f cutme.txt ]; then
+  echo did not paste file
+  exit 1
+fi
+if [ ! -f cutdummydir/cutme.txt ]; then
   echo did not paste file in directory
   exit 1
 fi
@@ -32,12 +59,12 @@ cd ..
 rm -rf dummydir2
 
 #test copying a file and a directory to clipboard 1
-clipboard copy1 dummy.txt dummydir
-if [ ! -f "$TMPDIR"/Clipboard/1/dummy.txt ]; then
+clipboard copy1 copyme.txt dummydir
+if [ ! -f "$TMPDIR"/Clipboard/1/copyme.txt ]; then
   echo did not copy file into cb 1
   exit 1
 fi
-if [ ! -f "$TMPDIR"/Clipboard/1/dummydir/foo.txt ]; then
+if [ ! -f "$TMPDIR"/Clipboard/1/dummydir/copyme.txt ]; then
   echo did not copy directory into cb 1
   exit 1
 fi
@@ -46,11 +73,40 @@ fi
 mkdir dummydir2
 cd dummydir2
 clipboard paste1
-if [ ! -f dummy.txt ]; then
+if [ ! -f copyme.txt ]; then
   echo did not paste file from cb 1
   exit 1
 fi
-if [ ! -f dummydir/foo.txt ]; then
+if [ ! -f dummydir/copyme.txt ]; then
+  echo did not paste file in directory from cb 1
+  exit 1
+fi
+
+#setup for more cut tests
+echo "Foobar" > cutme.txt
+mkdir cutdummydir
+echo "Foobar" > dummydir/cutme.txt
+
+#test cutting a file and a directory to clipboard 1
+cd ..
+clipboard cut1 cutme.txt cutdummydir
+if [ ! -f "$TMPDIR"/Clipboard/1/cutme.txt ]; then
+  echo did not cut file into cb 1
+  exit 1
+fi
+if [ ! -f "$TMPDIR"/Clipboard/1/cutdummydir/cutme.txt ]; then
+  echo did not cut directory into cb 1
+  exit 1
+fi
+
+#test pasting a file and a directory that were cut from clipboard 1
+cd dummydir2
+clipboard paste1
+if [ ! -f cutme.txt ]; then
+  echo did not paste file from cb 1
+  exit 1
+fi
+if [ ! -f cutdummydir/cutme.txt ]; then
   echo did not paste file in directory from cb 1
   exit 1
 fi
