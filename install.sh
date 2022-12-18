434bdd05302ed9ceffda470ef9e6699490121379
gentoo-btw <119129086+gentoo-btw@users.noreply.github.com>
2022-12-18 22:41:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,3 +26,16 @@ exec cmake --build .
 printf "=> sudo cmake --install .\n"
 exec sudo cmake --install .
 fi
+
+if [[ "$OSTYPE" == "openbsd"* ]]; then
+printf "=> Clipboard for OpenBSD Installer\n"
+printf "=> Made by gentoo-btw\n"
+printf " \n"
+read -p "=> You are about to install Clipboard for OpenBSD. Press ENTER to install Clipboard.."
+printf "=> cmake Clipboard/src\n"
+exec cmake Clipboard/src
+printf "=> cmake --build .\n"
+exec cmake --build .
+printf "=> doas cmake --install .\n"
+exec sudo cmake --install .
+fi
