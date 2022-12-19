625c511d9f9d40c062e491a09a1810eff21335eb
gentoo-btw <119129086+gentoo-btw@users.noreply.github.com>
2022-12-19 13:29:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ printf "=> cmake --build .\n"
 exec cmake --build .
 printf "=> sudo cmake --install .\n"
 exec sudo cmake --install .
+exit 0
 fi
 
 if [[ "$OSTYPE" == "freebsd"* ]]; then
@@ -25,6 +26,7 @@ printf "=> cmake --build .\n"
 exec cmake --build .
 printf "=> sudo cmake --install .\n"
 exec sudo cmake --install .
+exit 0
 fi
 
 if [[ "$OSTYPE" == "openbsd"* ]]; then
@@ -38,6 +40,7 @@ printf "=> cmake --build .\n"
 exec cmake --build .
 printf "=> doas cmake --install .\n"
 exec sudo cmake --install .
+exit 0
 fi
 
 if [[ "$OSTYPE" == "solaris"* ]]; then
@@ -51,4 +54,19 @@ printf "=> cmake --build .\n"
 exec cmake --build .
 printf "=> sudo cmake --install .\n"
 exec sudo cmake --install .
+exit 0
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+printf "=> Clipboard for Mac Installer\n"
+printf "=> Made by gentoo-btw\n"
+read -p "=> You are about to install Clipboard for macOS. Press ENTER to install Clipboard.."
+printf "=> NOTE: sudo is required. if you don't have sudo. cmake will fail to install Clipboard."
+printf "=> cmake Clipboard/src\n"
+exec cmake Clipboard/src
+printf "=> cmake --build .\n"
+exec cmake --build .
+printf "=> sudo cmake --install .\n"
+exec sudo cmake --install .
+exit 0
 fi
