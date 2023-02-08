1f9ce43aa480e89bffa4f8509ad59c094697cf94
o69mar <119129086+o69mar@users.noreply.github.com>
2023-02-08 19:20:49 +0300

    Add files via upload

--- src/install.sh
+++ src/install.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 set -eux
 
+if [ ! -f ~/.local/bin ]
+then
+    mkdir -p ~/.local/bin
+fi
+
+if [ -f /run/current-system/nixos-version ]
+then
+    tmp_dir=$(mktemp -d -t cb-XXXXXXXXXX)
+    cd $tmp_dir
+    sudo git clone https://github.com/Slackadays/Clipboard
+    cd Clipboard
+    sudo cmake .
+    sudo cmake --build .
+    sudo cp clipboard ~/.local/bin
+    sudo ln -s ~/.local/bin/clipboard ~/.local/bin/cb 
+    echo -e "\e[0;33mMake sure to add Clipboard to your PATH!\e[0m"
+    exit 0
+fi
+
 if [ "$(uname)" = "Linux" ]
 then
     tmp_dir=$(mktemp -d -t cb-XXXXXXXXXX)
@@ -56,12 +75,6 @@ then
     echo "Installed Clipboard"
     exit 0
 fi
-
-git clone --depth 1 https://github.com/slackadays/Clipboard
-
-pushd Clipboard/build
-cmake ..
-cmake --build .
 
 if [ "$(uname)" = "OpenBSD" ] #check if OpenBSD
 then
