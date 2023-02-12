d00027c24c09924a8dc5f0c98189a919a01e25d6
Slackadays <lightingdzeyenr@gmail.com>
2023-02-12 11:33:08 -0500

    The nuclear option

--- src/install.sh
+++ src/install.sh
@@ -1,39 +1,12 @@
 #!/bin/sh
 set -eux
 
-check_installation() {
-    #check if we can run the clipboard command
-    if [ ! -x "$(command -v clipboard)" ]
-    then
-        printf "\e[1;31mCouldn't install Clipboard\e[0m"
-        exit 1
-    else
-        printf "\e[1;32mClipboard installed successfully!\e[0m"
-        exit 0
-    fi
-}
-
-compile_section() {
-    if [ "$(nix-info --host-os > info.txt && grep -ow "NixOS" info.txt)" = "NixOS" ]
-    then
-        printf "\e[1;32mCompiling Clipboard for NixOS\e[0m"
-    fi
-
+compile() {
     git clone --depth 1 https://github.com/slackadays/Clipboard
     cd Clipboard/build
     cmake ..
     cmake --build .
-
-    if [ "$(nix-info --host-os > info.txt && grep -ow "NixOS" info.txt)" = "NixOS" ]
-    then
-        mkdir -p ~/.local/bin
-        sudo cp clipboard ~/.local/bin
-        sudo ln -s ~/.local/bin/clipboard ~/.local/bin/cb
-        export PATH="$HOME/.local/bin:$PATH"
-        printf "\e[1;33mMake sure to add Clipboard to your PATH!\e[0m"
-    else
-        cmake --install .
-    fi
+    cmake --install .
 
     if [ "$(uname)" = "OpenBSD" ] #check if OpenBSD
     then
@@ -41,18 +14,11 @@ compile_section() {
     else
         sudo cmake --install .
     fi
+    printf "\033[32mClipboard installed successfully!\033[0m"
 }
 
 tmp_dir=$(mktemp -d -t cb-XXXXXXXXXX)
 cd "$tmp_dir"
-
-#if [ "$(nix-info --host-os > info.txt && grep -ow "NixOS" info.txt)" = "NixOS" ]
-#then
-#    compile_section
-#    nix=true
-#else
-#    nix=false
-#fi
 
 if [ "$(uname)" = "Linux" ] && [ "$nix" = "false" ]
 then
@@ -93,10 +59,9 @@ then
         then
             sudo mv lib/libclipboardwayland.so /usr/lib/libclipboardwayland.so
         fi
+        printf "\033[32mClipboard installed successfully!\033[0m"
     fi
-fi
-
-if [ "$(uname)" = "Darwin" ]
+elif [ "$(uname)" = "Darwin" ]
 then
     curl -SsLl https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-macos-arm64-amd64.zip -o clipboard-macos.zip
     unzip clipboard-macos.zip
@@ -104,95 +69,23 @@ then
     sudo mv bin/clipboard /usr/local/bin/clipboard
     chmod +x /usr/local/bin/clipboard
     sudo ln -sf /usr/local/bin/clipboard /usr/local/bin/cb
-fi
-
-if [ "$(uname)" = "FreeBSD" ]
+    printf "\033[32mClipboard installed successfully!\033[0m"
+elif [ "$(uname)" = "FreeBSD" ]
 then
-    if [ "$(uname -m)" = "amd64" ]
-    then
-        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-freebsd-amd64.zip
-    else
-        download_link="skip"
-    fi
-    if [ "$download_link" != "skip" ]
-    then
-        curl -SsLl $download_link -o clipboard-freebsd.zip
-        unzip clipboard-freebsd.zip
-        rm clipboard-freebsd.zip
-        sudo mv bin/clipboard /usr/local/bin/clipboard
-        if [ -f "lib/libclipboardx11.so" ]
-        then
-            sudo mv lib/libclipboardx11.so /usr/local/lib/libclipboardx11.so
-        fi
-        if [ -f "lib/libclipboardwayland.so" ]
-        then
-            sudo mv lib/libclipboardwayland.so /usr/local/lib/libclipboardwayland.so
-        fi
-        chmod +x /usr/local/bin/clipboard
-        sudo ln -sf /usr/local/bin/clipboard /usr/local/bin/cb
-    fi
-fi
-
-if [ "$(uname)" = "OpenBSD" ]
+    printf "\033[31mSorry, but the installer script doesn't support FreeBSD yet.\n\033[0m"
+    printf '\033[32m👉 However, you can still install Clipboard with the other methods in the readme!\n\033[0m'
+elif [ "$(uname)" = "OpenBSD" ]
 then
-    if [ "$(uname -m)" = "amd64" ]
-    then
-        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-openbsd-amd64.zip
-    elif [ "$(uname -m)" = "arm64" ]
-    then
-        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-openbsd-arm64.zip
-    else
-        download_link="skip"
-    fi
-    if [ "$download_link" != "skip" ]
-    then
-        curl -SsLl $download_link -o clipboard-openbsd.zip
-        tar -xvf clipboard-openbsd.zip
-        rm clipboard-openbsd.zip
-        doas mv bin/clipboard /usr/local/bin/clipboard
-        if [ -f "lib/libclipboardx11.so" ]
-        then
-            doas mv lib/libclipboardx11.so /usr/local/lib/libclipboardx11.so
-        fi
-        if [ -f "lib/libclipboardwayland.so" ]
-        then
-            doas mv lib/libclipboardwayland.so /usr/local/lib/libclipboardwayland.so
-        fi
-        chmod +x /usr/local/bin/clipboard
-        doas ln -sf /usr/local/bin/clipboard /usr/local/bin/cb
-    fi
-fi
-
-if [ "$(uname)" = "NetBSD" ]
+    printf "\033[31mSorry, but the installer script doesn't support OpenBSD yet.\n\033[0m"
+    printf '\033[32m👉 However, you can still install Clipboard with the other methods in the readme!\n\033[0m'
+elif [ "$(uname)" = "NetBSD" ]
 then
-    if [ "$(uname -m)" = "amd64" ]
-    then
-        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-netbsd-amd64.zip
-    else
-        download_link="skip"
-    fi
-    if [ "$download_link" != "skip" ]
-    then
-        curl -SsLl $download_link -o clipboard-netbsd.zip
-        tar -xvf clipboard-netbsd.zip
-        rm clipboard-netbsd.zip
-        sudo mv bin/clipboard /usr/pkg/bin/clipboard
-        if [ -f "lib/libclipboardx11.so" ]
-        then
-            sudo mv lib/libclipboardx11.so /usr/pkg/lib/libclipboardx11.so
-        fi
-        if [ -f "lib/libclipboardwayland.so" ]
-        then
-            sudo mv lib/libclipboardwayland.so /usr/pkg/lib/libclipboardwayland.so
-        fi
-        chmod +x /usr/pkg/bin/clipboard
-        sudo ln -sf /usr/pkg/bin/clipboard /usr/pkg/bin/cb
-    fi
+    printf "\033[31mSorry, but the installer script doesn't support NetBSD yet.\n\033[0m"
+    printf '\033[32m👉 However, you can still install Clipboard with the other methods in the readme!\n\033[0m'
+else
+compile
 fi
-
-#compile_section
 
 rm -rf "$tmp_dir"
-check_installation
 
 cd ..
