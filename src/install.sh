b6a2407e21201d6c184a0780f9f0e1d4c3665917
o69mar <119129086+o69mar@users.noreply.github.com>
2023-02-17 23:50:59 +0300

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -24,9 +24,6 @@ verify() {
     fi
 }
 
-tmp_dir=$(mktemp -d -t cb-XXXXXXXXXX)
-cd "$tmp_dir"
-
 compile_nixos() {
     printf "%s" "\e[1;32mInstalling Clipboard for NixOS...\e[0m"
     git clone --depth 1 https://github.com/slackadays/Clipboard
@@ -40,70 +37,6 @@ compile_nixos() {
     printf "%s" "\e[1;32mClipboard installed successfully!\e[0m"
     rm -rfv "$tmp_dir"
     exit 0
-}
-
-install_precompiled_section() {
-    if [ "$(uname -m)" = "x86_64" ]
-    then
-        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-amd64.zip
-    elif [ "$(uname -m)" = "aarch64" ]
-    then
-        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-arm64.zip
-    elif [ "$(uname -m)" = "riscv64" ]
-    then
-        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-riscv64.zip
-    elif [ "$(uname -m)" = "i386" ]
-    then
-        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-i386.zip
-    elif [ "$(uname -m)" = "ppc64le" ]
-    then
-        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-ppc64le.zip
-    elif [ "$(uname -m)" = "s390x" ]
-    then
-        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-s390x.zip
-    elif [ "$(uname)" = "Darwin" ]
-    then
-        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-macos-arm64-amd64.zip -o clipboard-macos.zip
-    else
-        download_link="skip"
-    fi
-
-    if ! unzip 
-    then
-        ZIP_ARCHIVER="7z"
-    else
-        ZIP_ARCHIVER="unzip"
-    fi
-
-    if ! 7z 
-    then
-        echo "Please install a .zip extracter, such as 7z or unzip."
-        exit 1
-    fi
-
-     if [ "$download_link" != "skip" ]
-     then
-        curl -SL $download_link -o clipboard.zip
-        $ZIP_EXTRACTER clipboard.zip
-        rm clipboard.zip
-        sudo mv bin/clipboard /usr/local/bin/clipboard
-        chmod +x /usr/local/bin/clipboard
-        sudo ln -sf /usr/local/bin/clipboard /usr/local/bin/cb
-    fi
-
-    if [ "$download_link" = "skip" ]
-    then
-        compile
-    fi
-
-     if [[ -f "lib/libclipboardx11.so" && ! -f "lib/libclipboardx11.so" ]]
-     then
-            sudo mv lib/libclipboardx11.so /usr/lib/libclipboardx11.so
-     fi
-     if [[ -f "lib/libclipboardwayland.so" && ! -f "lib/libclipboardwayland.so" ]]
-     then
-            sudo mv lib/libclipboardwayland.so /usr/lib/libclipboardwayland.so
-     fi
 }
 
 compile() {
@@ -132,16 +65,62 @@ compile() {
      fi
 }
 
+
+tmp_dir=$(mktemp -d -t cb-XXXXXXXXXX)
+cd "$tmp_dir"
+
 if [ "$(nix-info --host-os > info.txt && grep -ow "NixOS" info.txt)" = "NixOS" ]; then
     compile_nixos
 fi
 
 if [ "$(uname)" = "Linux" ]
 then
-    install_precompiled_section
+    if [ "$(uname -m)" = "x86_64" ]
+    then
+        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-amd64.zip
+    elif [ "$(uname -m)" = "aarch64" ]
+    then
+        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-arm64.zip
+    elif [ "$(uname -m)" = "riscv64" ]
+    then
+        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-riscv64.zip
+    elif [ "$(uname -m)" = "i386" ]
+    then
+        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-i386.zip
+    elif [ "$(uname -m)" = "ppc64le" ]
+    then
+        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-ppc64le.zip
+    elif [ "$(uname -m)" = "s390x" ]
+    then
+        download_link=https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-linux-s390x.zip
+    else
+        download_link="skip"
+    fi
+    if [ "$download_link" != "skip" ]
+    then
+        curl -SL $download_link -o clipboard-linux.zip
+        unzip clipboard-linux.zip
+        rm clipboard-linux.zip
+        sudo mv bin/clipboard /usr/bin/clipboard
+        chmod +x /usr/bin/clipboard
+        sudo ln -sf /usr/bin/clipboard /usr/bin/cb
+        if [ -f "lib/libclipboardx11.so" ]
+        then
+            sudo mv lib/libclipboardx11.so /usr/lib/libclipboardx11.so
+        fi
+        if [ -f "lib/libclipboardwayland.so" ]
+        then
+            sudo mv lib/libclipboardwayland.so /usr/lib/libclipboardwayland.so
+        fi
+    fi
 elif [ "$(uname)" = "Darwin" ]
 then
-    install_precompiled_section
+    curl -SsLl https://nightly.link/Slackadays/Clipboard/workflows/main/main/clipboard-macos-arm64-amd64.zip -o clipboard-macos.zip
+    unzip clipboard-macos.zip
+    rm clipboard-macos.zip
+    sudo mv bin/clipboard /usr/local/bin/clipboard
+    chmod +x /usr/local/bin/clipboard
+    sudo ln -sf /usr/local/bin/clipboard /usr/local/bin/cb
 elif [ "$(uname)" = "FreeBSD" ]
 then
     unsupported "FreeBSD"
