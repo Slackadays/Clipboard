912a1daa89eba7f15a821e91a44ecf925c6e511f
Slackadays <lightingdzeyenr@gmail.com>
2023-02-16 22:37:07 -0500

    Use emojis instead

--- src/clipboard/src/messages.cpp
+++ src/clipboard/src/messages.cpp
@@ -68,40 +68,40 @@ Message no_clipboard_contents_message = "[blue]• There is currently nothing in
 Message clipboard_action_prompt = "[pink]Add [bold]cut, copy, [blank][pink]or[bold] paste[blank][pink] to the end, "
                                   "like [bold]clipboard copy[blank][pink] to get started, or if you need help, try "
                                   "[bold]clipboard -h[blank][pink] to show the help screen.[blank]\n";
-Message no_valid_action_message = "[red]╳ You did not specify a valid action ([bold]\"%s\"[blank][red]), or you forgot "
+Message no_valid_action_message = "[red]❌ You did not specify a valid action ([bold]\"%s\"[blank][red]), or you forgot "
                                   "to include one. [pink]Try using or adding [bold]cut, copy, [blank][pink]or "
                                   "[bold]paste[blank][pink] instead, like [bold]clipboard copy.[blank]\n";
-Message choose_action_items_message = "[red]╳ You need to choose something to %s.[pink] Try adding the items you want "
+Message choose_action_items_message = "[red]❌ You need to choose something to %s.[pink] Try adding the items you want "
                                       "to %s to the end, like [bold]clipboard %s contacts.txt myprogram.cpp[blank]\n";
-Message fix_redirection_action_message = "[red]╳ You can't use the [bold]%s[blank][red] action with redirection here. [pink]Try removing "
+Message fix_redirection_action_message = "[red]❌ You can't use the [bold]%s[blank][red] action with redirection here. [pink]Try removing "
                                          "[bold]%s[blank][pink] or use [bold]%s[blank][pink] instead, like [bold]clipboard %s[blank][pink].\n";
-Message redirection_no_items_message = "[red]╳ You can't specify items when you use redirection. [pink]Try removing "
+Message redirection_no_items_message = "[red]❌ You can't specify items when you use redirection. [pink]Try removing "
                                        "the items that come after [bold]clipboard [action].\n";
-Message paste_success_message = "[green]✓ Pasted successfully[blank]\n";
-Message clipboard_failed_one_message = "[red]╳ Clipboard couldn't %s this item:[blank]\n";
-Message clipboard_failed_many_message = "[red]╳ Clipboard couldn't %s these items:[blank]\n";
+Message paste_success_message = "[green]✅ Pasted successfully[blank]\n";
+Message clipboard_failed_one_message = "[red]❌ Clipboard couldn't %s this item:[blank]\n";
+Message clipboard_failed_many_message = "[red]❌ Clipboard couldn't %s these items:[blank]\n";
 Message and_more_fails_message = "[red]▏ ...and [bold]%i[blank][red] more.[blank]\n";
 Message and_more_items_message = "[blue]▏ ...and [bold]%i[blank][blue] more.[blank]\n";
 Message fix_problem_message = "[pink]▏ See if you have the needed permissions, or\n"
                               "▏ try double-checking the spelling of the files or what directory you're in.[blank]\n";
-Message not_enough_storage_message = "[red]╳ There won't be enough storage available to paste everything (%gkB to "
+Message not_enough_storage_message = "[red]❌ There won't be enough storage available to paste everything (%gkB to "
                                      "paste, %gkB available).[blank][pink] Try double-checking what items you've "
                                      "selected or delete some files to free up space.[blank]\n";
 Message item_already_exists_message = "[yellow]• The item [bold]%s[blank][yellow] already exists here. Do you want to "
                                       "replace it? [pink]Use [bold]all [blank][pink]to replace all existing, or "
                                       "[bold]skip[blank][pink] to replace nothing. [bold][(y)es/(n)o)/(a)ll/(s)kip] ";
-Message bad_response_message = "[red]╳ Sorry, that wasn't a valid choice. Try again: [blank][pink][bold][(y)es/(n)o)] ";
+Message bad_response_message = "[red]❌ Sorry, that wasn't a valid choice. Try again: [blank][pink][bold][(y)es/(n)o)] ";
 Message working_message = "[yellow]• %s... %llu%s %s[blank]\r";
-Message cancelled_message = "[green]✓ Cancelled %s[blank]";
-Message byte_success_message = "[green]✓ %s %llu bytes[blank]\n";
-Message one_item_success_message = "[green]✓ %s %s[blank]\n";
-Message many_files_success_message = "[green]✓ %s %lu files[blank]\n";
-Message many_directories_success_message = "[green]✓ %s %lu directories[blank]\n";
-Message one_file_one_directory_success_message = "[green]✓ %s one file and one directory[blank]\n";
-Message one_file_many_directories_success_message = "[green]✓ %s one file and %lu directories[blank]\n";
-Message many_files_one_directory_success_message = "[green]✓ %s %lu files and one directory[blank]\n";
-Message many_files_many_directories_success_message = "[green]✓ %s %lu files and %lu directories[blank]\n";
-Message internal_error_message = "[red]╳ Internal error: %s\n▏ This is probably a bug, or you might be lacking "
+Message cancelled_message = "[green]✅ Cancelled %s[blank]";
+Message byte_success_message = "[green]✅ %s %llu bytes[blank]\n";
+Message one_item_success_message = "[green]✅ %s %s[blank]\n";
+Message many_files_success_message = "[green]✅ %s %lu files[blank]\n";
+Message many_directories_success_message = "[green]✅ %s %lu directories[blank]\n";
+Message one_file_one_directory_success_message = "[green]✅ %s one file and one directory[blank]\n";
+Message one_file_many_directories_success_message = "[green]✅ %s one file and %lu directories[blank]\n";
+Message many_files_one_directory_success_message = "[green]✅ %s %lu files and one directory[blank]\n";
+Message many_files_many_directories_success_message = "[green]✅ %s %lu files and %lu directories[blank]\n";
+Message internal_error_message = "[red]❌ Internal error: %s\n▏ This is probably a bug, or you might be lacking "
                                  "permissions on this system.[blank]\n";
 
 void setLanguageES() {
@@ -141,7 +141,7 @@ void setLanguageES() {
                    "[blue]▏Copyright (C) 2022 Jackson Huff. Licensed under the GPLv3.[blank]\n"
                    "[blue]▏This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome "
                    "to redistribute it under certain conditions.[blank]\n";
-    no_valid_action_message = "[red]╳ No especificaste ninguna acción válida o se te olvidó. [pink]Inténta usar o añadir [bold]cortar, "
+    no_valid_action_message = "[red]❌ No especificaste ninguna acción válida o se te olvidó. [pink]Inténta usar o añadir [bold]cortar, "
                               "copiar o pegar[blank][pink] en su lugar, como [bold]clipboard copiar.[blank]\n";
     clipboard_item_many_contents_message = "[blue]• Aquí están las [bold]%i[blank][blue] cosas primeras del "
                                            "portapapeles [bold]%s[blank][blue]: [blank]\n";
@@ -149,26 +149,26 @@ void setLanguageES() {
     clipboard_action_prompt = "[pink]Añade [bold]cortar, copiar [blank][pink]o[bold] pegar[blank][pink] al final, como "
                               "[bold]clipboard copiar[blank][pink] para comenzar, o si necesitas ayuda, haz "
                               "[bold]clipboard -h[blank][pink] para mostrar el mensaje de ayudar.[blank]\n";
-    choose_action_items_message = "[red]╳ Necesitas escoger una cosa para %s.[pink] Inténta añadir las cosas que "
+    choose_action_items_message = "[red]❌ Necesitas escoger una cosa para %s.[pink] Inténta añadir las cosas que "
                                   "quieres %s al final, como [bold]clipboard %s contactos.txt miprograma.cpp[blank]\n";
-    fix_redirection_action_message = "[red]╳ No se puede usar la acción [bold]%s[blank][red] con la redirección. [pink]Inténta sacar "
+    fix_redirection_action_message = "[red]❌ No se puede usar la acción [bold]%s[blank][red] con la redirección. [pink]Inténta sacar "
                                      "[bold]%s[blank][pink] o usa [bold]%s[blank][pink] en su lugar, como [bold]clipboard %s[blank][pink].\n";
-    redirection_no_items_message = "[red]╳ No se pueden especificar las cosas con redirección. [pink]Inténta sacar las "
+    redirection_no_items_message = "[red]❌ No se pueden especificar las cosas con redirección. [pink]Inténta sacar las "
                                    "cosas que siguen [bold]clipboard [acción].\n";
-    paste_success_message = "[green]✓ Pegó con éxito[blank]\n";
-    clipboard_failed_many_message = "[red]╳ Clipboard no pudo %s estas cosas.[blank]\n";
+    paste_success_message = "[green]✅ Pegó con éxito[blank]\n";
+    clipboard_failed_many_message = "[red]❌ Clipboard no pudo %s estas cosas.[blank]\n";
     and_more_fails_message = "[red]▏ ...y [bold]%i[blank][red] más.[blank]\n";
     and_more_items_message = "[blue]▏ ...y [bold]%i[blank][blue] más.[blank]\n";
     fix_problem_message = "[pink]▏ Verífica si tengas los permisos necesarios, o\n"
                           "▏ vuelve a revisar el deletro de los archivos o la carpeta en que estás.[blank]\n";
-    not_enough_storage_message = "[red]╳ No habrá espacio suficiente para pegar todas tus cosas (%gkB a pegar, %gkB "
+    not_enough_storage_message = "[red]❌ No habrá espacio suficiente para pegar todas tus cosas (%gkB a pegar, %gkB "
                                  "disponible).[blank][pink] Vuelve a revisar las cosas que especificaste o saca "
                                  "algunas cosas para hacer más espacio.[blank]\n";
-    byte_success_message = "[green]✓ %s %llu bytes[blank]\n";
-    many_files_success_message = "[green]✓ %s %lu archivos[blank]\n";
-    many_directories_success_message = "[green]✓ %s %lu carpetas[blank]\n";
-    many_files_many_directories_success_message = "[green]✓ %s %lu archivos y %lu carpetas[blank]\n";
-    internal_error_message = "[red]╳ Error internal: %s[blank]\n";
+    byte_success_message = "[green]✅ %s %llu bytes[blank]\n";
+    many_files_success_message = "[green]✅ %s %lu archivos[blank]\n";
+    many_directories_success_message = "[green]✅ %s %lu carpetas[blank]\n";
+    many_files_many_directories_success_message = "[green]✅ %s %lu archivos y %lu carpetas[blank]\n";
+    internal_error_message = "[red]❌ Error internal: %s[blank]\n";
 }
 
 void setLanguagePT() {
@@ -200,28 +200,28 @@ void setLanguagePT() {
                    "[blue]▏Copyright (C) 2022 Jackson Huff. Licensed under the GPLv3.[blank]\n"
                    "[blue]▏Este programa vem com ABSOLUTAMENTE NENHUMA GARANTIA. Este é um software livre, e você é "
                    "bem-vindo a redistribuí-lo sob certas condições.[blank]\n";
-    no_valid_action_message = "[red]╳ Você não especificou uma ação válida (\"%s\"), ou esqueceu de incluí-la. [pink]Tente utilizar "
+    no_valid_action_message = "[red]❌ Você não especificou uma ação válida (\"%s\"), ou esqueceu de incluí-la. [pink]Tente utilizar "
                               "[bold]recortar, copiar ou colar[blank][pink], como em [bold]clipboard copiar.[blank]\n";
-    no_clipboard_contents_message = "[red]╳ Você não especificou uma ação válida. [pink]Tente adicionar [bold]recortar, copiar, or "
+    no_clipboard_contents_message = "[red]❌ Você não especificou uma ação válida. [pink]Tente adicionar [bold]recortar, copiar, or "
                                     "colar[blank][pink] no final, como em [bold]clipboard copiar[blank][pink]. Caso precise de ajuda, tente "
                                     "[bold]clipboard -h[blank][pink] para mostrar a tela de instruções.[blank]\n";
-    choose_action_items_message = "[red]╳ Você precisa especificar algo para %s.[pink] Tenta adicionar os itens que você quer %s ao final, "
+    choose_action_items_message = "[red]❌ Você precisa especificar algo para %s.[pink] Tenta adicionar os itens que você quer %s ao final, "
                                   "como em [bold]clipboard %s contatos.txt meuprograma.cpp[blank]\n";
-    fix_redirection_action_message = "[red]╳ Você não pode [bold]%s[blank][red] com redirecionamento aqui. [pink]Tente remover "
+    fix_redirection_action_message = "[red]❌ Você não pode [bold]%s[blank][red] com redirecionamento aqui. [pink]Tente remover "
                                      "[bold]%s[blank][pink] ou utilizar [bold]%s[blank][pink], como em [bold]clipboard %s[blank][pink].\n";
-    redirection_no_items_message = "[red]╳ Você não pode especificar itens ao redirecionar. [pink]Tente remover itens "
+    redirection_no_items_message = "[red]❌ Você não pode especificar itens ao redirecionar. [pink]Tente remover itens "
                                    "que vêm após [bold]clipboard [action].\n";
-    paste_success_message = "[green]✓ Colado com sucesso[blank]\n";
-    clipboard_failed_many_message = "[red]╳ Clipboard não pôde %s esses itens.[blank]\n";
+    paste_success_message = "[green]✅ Colado com sucesso[blank]\n";
+    clipboard_failed_many_message = "[red]❌ Clipboard não pôde %s esses itens.[blank]\n";
     and_more_fails_message = "[red]▏ ...e mais [bold]%i[blank][red].[blank]\n";
     and_more_items_message = "[blue]▏ ...e mais [bold]%i[blank][blue].[blank]\n";
     fix_problem_message = "[pink]▏ Veja se você possui as permissões necessárias, ou\n"
                           "▏ verifique a ortografia do arquivo ou diretório que voce está.[blank]\n";
-    byte_success_message = "[green]✓ %s %llu bytes[blank]\n";
-    many_files_success_message = "[green]✓ %s %lu arquivos[blank]\n";
-    many_directories_success_message = "[green]✓ %s %lu diretórios[blank]\n";
-    many_files_many_directories_success_message = "[green]✓ %s %lu arquivos e %lu diretórios[blank]\n";
-    internal_error_message = "[red]╳ Erro interno: %s\n▏ Isso é provavelmente um bug.[blank]\n";
+    byte_success_message = "[green]✅ %s %llu bytes[blank]\n";
+    many_files_success_message = "[green]✅ %s %lu arquivos[blank]\n";
+    many_directories_success_message = "[green]✅ %s %lu diretórios[blank]\n";
+    many_files_many_directories_success_message = "[green]✅ %s %lu arquivos e %lu diretórios[blank]\n";
+    internal_error_message = "[red]❌ Erro interno: %s\n▏ Isso é provavelmente um bug.[blank]\n";
 }
 
 void setLanguageTR() {
@@ -277,27 +277,27 @@ void setLanguageTR() {
     clipboard_action_prompt = "[pink]Başlamak için sonuna [bold]kes, kopyala, [blank][pink]veya[bold] yapistir[blank][pink] ekleyin, "
                               "[bold]clipboard kopyala[blank][pink] gibi, veya yardıma ihtiyacın olursa yardım ekranını göstermek için "
                               "[bold]clipboard -h[blank][pink]'i dene.[blank]\n";
-    no_valid_action_message = "[red]╳ Geçerli bir işlem vermediniz (\"%s\") veya işlem vermeyi unuttunuz [pink]Komutunuza [bold]cut, "
+    no_valid_action_message = "[red]❌ Geçerli bir işlem vermediniz (\"%s\") veya işlem vermeyi unuttunuz [pink]Komutunuza [bold]cut, "
                               "copy, [blank][pink]ya da [bold]paste[blank][pink] eklemelisiniz, örneğin [bold]clipboard copy.[blank]\n";
-    choose_action_items_message = "[red]╳ %s(ma/me) işlemi için bir öğe seçmeniz gerekmektedir.[pink] %s işleminden sonra öğeler eklemeyi "
+    choose_action_items_message = "[red]❌ %s(ma/me) işlemi için bir öğe seçmeniz gerekmektedir.[pink] %s işleminden sonra öğeler eklemeyi "
                                   "deneyiniz, örneğin [bold]clipboard %s contacts.txt myprogram.cpp[blank]\n";
-    fix_redirection_action_message = "[red]╳ [bold]%s[blank][red](ma/me) işlemini burada yeniden yönlendirme ile kullanamazsın. [pink] "
+    fix_redirection_action_message = "[red]❌ [bold]%s[blank][red](ma/me) işlemini burada yeniden yönlendirme ile kullanamazsın. [pink] "
                                      "[bold]%s[blank][pink] işlemini silin veya [bold]%s[blank][pink] işlemini kullanın, örneğin "
                                      "[bold]clipboard %s[blank][pink].\n";
-    redirection_no_items_message = "[red]╳ Yeniden yönlendirme işlemi yaparken öğe veremezsiniz. [pink][bold]clipboard "
+    redirection_no_items_message = "[red]❌ Yeniden yönlendirme işlemi yaparken öğe veremezsiniz. [pink][bold]clipboard "
                                    "[action][blank][pink]'dan sonra  gelen öğeleri siliniz.\n";
-    paste_success_message = "[green]✓ Yapıştırma başarıyla tamamlandı[blank]\n";
-    clipboard_failed_many_message = "[red]╳ %s(ma/me) işlemi şu öğeler için başarısız oldu:[blank]\n";
+    paste_success_message = "[green]✅ Yapıştırma başarıyla tamamlandı[blank]\n";
+    clipboard_failed_many_message = "[red]❌ %s(ma/me) işlemi şu öğeler için başarısız oldu:[blank]\n";
     and_more_fails_message = "[red]▏ ...ve [bold]%i[blank][red] fazla.[blank]\n";
     and_more_items_message = "[blue]▏ ...ve [bold]%i[blank][blue] fazla.[blank]\n";
     fix_problem_message = "[pink]▏ Erişime ihtiyacınız varsa şuna bakın, veya\n"
                           "▏ bulunduğunuz dizini veya girdiğiniz dosya isimlerini ikinci kez kontrol edin.[blank]\n";
-    not_enough_storage_message = "[red]╳ Bütün öğelerinizi yapıştırabileceğin kadar yeterli bir alanınız yok (%gkB "
+    not_enough_storage_message = "[red]❌ Bütün öğelerinizi yapıştırabileceğin kadar yeterli bir alanınız yok (%gkB "
                                  "yapıştırılacak, %gkB boş).[blank][pink] Hangi öğeleri seçtiğinizi ikinci kez kontrol "
                                  "etmeyi deneyin veya yer açmak için bazı dosyaları silin.[blank]\n";
-    byte_success_message = "[green]✓ %s %llu bayt[blank]\n";
-    many_files_success_message = "[green]✓ %s %lu dosya[blank]\n";
-    many_directories_success_message = "[green]✓ %s %lu dizin[blank]\n";
-    many_files_many_directories_success_message = "[green]✓ %s %lu dosya ve %lu dizin[blank]\n";
-    internal_error_message = "[red]╳ İçsel hata: %s\n▏ Bu yüksek ihtimal bir hata veya bu sistemde erişim sorunu yaşıyorsunuz.[blank]\n";
+    byte_success_message = "[green]✅ %s %llu bayt[blank]\n";
+    many_files_success_message = "[green]✅ %s %lu dosya[blank]\n";
+    many_directories_success_message = "[green]✅ %s %lu dizin[blank]\n";
+    many_files_many_directories_success_message = "[green]✅ %s %lu dosya ve %lu dizin[blank]\n";
+    internal_error_message = "[red]❌ İçsel hata: %s\n▏ Bu yüksek ihtimal bir hata veya bu sistemde erişim sorunu yaşıyorsunuz.[blank]\n";
 }
