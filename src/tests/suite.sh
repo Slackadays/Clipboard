0b74b5e61708557a0d5cd32ac9d8006480693cf9
Slackadays <lightingdzeyenr@gmail.com>
2023-03-08 23:27:59 -0500

    Recursive testing now

--- src/tests/suite.sh
+++ src/tests/suite.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-set -eu
+set -u
 
-trap "rm -r test_*" 0
+clear_tempdirs() {
+    rm -rf test_*
+}
+
+trap clear_tempdirs 0
 
 if [ -z "${CLIPBOARD_TMPDIR:-}" ]
 then
@@ -14,25 +18,41 @@ BASEDIR="$(dirname "$0")"
 
 cd "$BASEDIR" || exit 1
 
-rm -rf test_*
+clear_tempdirs
 
-sh add-file.sh
-sh add-pipe.sh
-sh add-text.sh
-sh clear-file.sh
-sh clear-pipe.sh
-sh clear-text.sh
-sh copy-file.sh
-sh copy-pipe.sh
-sh copy-text.sh
-sh cut-file.sh
-sh cut-pipe.sh
-sh cut-text.sh
-sh paste-file.sh
-sh paste-pipe.sh
-sh paste-text.sh
-sh show-file.sh
-sh show-pipe.sh
-sh show-text.sh
+locales=(en_US.UTF-8 es_ES.UTF-8 pt_BR.UTF-8 tr_TR.UTF-8)
+themes=(dark light darkhighcontrast lighthighcontrast amber green)
+
+run_tests() {
+    sh add-file.sh
+    sh add-pipe.sh
+    sh add-text.sh
+    sh clear-file.sh
+    sh clear-pipe.sh
+    sh clear-text.sh
+    sh copy-file.sh
+    sh copy-pipe.sh
+    sh copy-text.sh
+    sh cut-file.sh
+    sh cut-pipe.sh
+    sh cut-text.sh
+    sh paste-file.sh
+    sh paste-pipe.sh
+    sh paste-text.sh
+    sh show-file.sh
+    sh show-pipe.sh
+    sh show-text.sh
+}
+
+#for locale in "${locales[@]}"
+#do 
+#    export LC_ALL="$locale"
+    for theme in "${themes[@]}"
+    do
+        export CLIPBOARD_THEME="$theme"
+        run_tests
+        clear_tempdirs
+    done
+#done
 
 echo "🐢 All tests passed!"
