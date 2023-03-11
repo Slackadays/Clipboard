68eb6e28fffaf8467369235100342dec74fcbdf2
Slackadays <Slackadays@users.noreply.github.com>
2023-03-10 20:52:27 -0500

    Add skip for other note test

--- src/tests/note-text.sh
+++ src/tests/note-text.sh
@@ -7,4 +7,11 @@ clipboard note "Foobar"
 
 unset CLIPBOARD_FORCETTY
 
+#skip macOS and Windows GHA
+if [ "$CI" = "true" ] && [ "$RUNNER_OS" = "macOS" ] || [ "$RUNNER_OS" = "Windows" ];
+then
+    echo "Skipping test on macOS CI due to Not A TTY bug"
+    exit 0
+fi
+
 assert_equals "Foobar" "$(clipboard note)"
