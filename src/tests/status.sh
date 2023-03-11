dfc7cd42bc30f22f02381e8adc33d59468072f1a
Slackadays <Slackadays@users.noreply.github.com>
2023-03-11 10:00:17 -0500

    Workaround just for Windows

--- src/tests/status.sh
+++ src/tests/status.sh
@@ -7,6 +7,14 @@ make_files
 
 clipboard copy testfile testdir
 
+# check CI is true and the runner is Windows
+set +u
+if [ "$CI" = "true" ] && [ "$RUNNER_OS" = "Windows" ]
+then
+    echo "Skipping test on Windows CI due to Not A TTY bug"
+    exit 0
+fi
+
 content="$(clipboard)"
 
 content_is_shown "$content" "testfile"
