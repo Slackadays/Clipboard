c13596f50f8d4a853e2aa5f052d91302598619fa
Slackadays <Slackadays@users.noreply.github.com>
2023-03-10 20:47:40 -0500

    Fix exiting

--- src/tests/note-pipe.sh
+++ src/tests/note-pipe.sh
@@ -7,7 +7,8 @@ echo "Foobar" | clipboard note
 # check CI is true and the runner is macOS
 if [ "$CI" = "true" ] && [ "$RUNNER_OS" = "macOS" ] || [ "$RUNNER_OS" = "Windows" ];
 then
-    pass_test "Skipping test on macOS CI due to Not A TTY bug"
+    echo "Skipping test on macOS CI due to Not A TTY bug"
+    exit 0
 fi
 
 assert_equals "Foobar" "$(clipboard note)"
