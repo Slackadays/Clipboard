ca6914341732fbca339effa0c64006f37148357c
Slackadays <Slackadays@users.noreply.github.com>
2023-03-25 23:41:12 -0400

    Fix clang-format

--- src/clipboard/src/clipboard.cpp
+++ src/clipboard/src/clipboard.cpp
@@ -757,8 +757,7 @@ void showSuccesses() {
 
 int main(int argc, char* argv[]) {
     try {
-        if (!strcmp(argv[0], "clipboard"))
-            std::cerr << "Warning! \"clipboard\" is now deprecated. Please use \"cb\" instead" << std::endl;
+        if (!strcmp(argv[0], "clipboard")) std::cerr << "Warning! \"clipboard\" is now deprecated. Please use \"cb\" instead" << std::endl;
 
         setupHandlers();
 
