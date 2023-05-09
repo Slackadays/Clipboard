52e0e3bf3a6a8cc3d5e386a099164c9f21496772
Slackadays <Slackadays@users.noreply.github.com>
2023-05-09 19:05:38 -0400

    Apply a fix for Sway?

--- src/tests/wayland.sh
+++ src/tests/wayland.sh
@@ -5,7 +5,7 @@ export CLIPBOARD_FORCETTY=1
 set +u
 
 # Test if we have wl-clipboard installed
-if ! command -v wl-paste >/dev/null 2>&1;
+if ! command -v wl-copy >/dev/null 2>&1;
 then
     echo "⏭️ Skipping Wayland tests due to missing wl-clipboard"
     exit 0
@@ -23,7 +23,7 @@ then
     exit 0
 fi
 
-if ! wl-paste >/dev/null 2>&1;
+if ! wl-copy "Foobar" && wl-paste >/dev/null 2>&1;
 then
     echo "⏭️ Skipping Wayland tests due to wl-clipboard failing"
     exit 0
