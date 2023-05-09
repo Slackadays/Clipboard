5eaaa02c0258015d403001964805dfb2acccbb64
Slackadays <Slackadays@users.noreply.github.com>
2023-05-09 18:37:44 -0400

    Put back Wayland for another time

--- src/tests/wayland.sh
+++ src/tests/wayland.sh
@@ -4,10 +4,10 @@ start_test "Test Wayland functionality"
 export CLIPBOARD_FORCETTY=1
 set +u
 
-# Test if we have wl-copy installed
-if ! command -v wl-copy >/dev/null 2>&1;
+# Test if we have wl-clipboard installed
+if ! command -v wl-paste >/dev/null 2>&1;
 then
-    echo "⏭️ Skipping Wayland tests due to missing wl-copy"
+    echo "⏭️ Skipping Wayland tests due to missing wl-clipboard"
     exit 0
 fi
 
@@ -23,9 +23,9 @@ then
     exit 0
 fi
 
-if ! wl-copy -o >/dev/null 2>&1;
+if ! wl-paste >/dev/null 2>&1;
 then
-    echo "⏭️ Skipping Wayland tests due to wl-copy failing"
+    echo "⏭️ Skipping Wayland tests due to wl-clipboard failing"
     exit 0
 fi
 
