c9132ff63745ed797a389fd4f2b464039eacb606
Slackadays <lightingdzeyenr@gmail.com>
2023-06-14 01:11:32 -0400

    Fix ALSA fallback

--- src/cb/src/alsaandfallback.cpp
+++ src/cb/src/alsaandfallback.cpp
@@ -13,9 +13,10 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
 #include "clipboard.hpp"
-#include <alsa/asoundlib.h>
 
 #if defined(USE_ALSA)
+#include <alsa/asoundlib.h>
+
 bool playAsyncSoundEffect(const std::valarray<short>& samples) {
     if (fork()) return true;
 
