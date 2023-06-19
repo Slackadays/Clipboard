529572b94e62b8e2fc2004c515e109b8e5440c30
Jackson Huff <lightingdzeyenr@gmail.com>
2023-06-19 17:38:14 -0400

    Re-do license blurb [skip ci]

--- src/cbwayland/src/objects/shm.hpp
+++ src/cbwayland/src/objects/shm.hpp
@@ -1,4 +1,4 @@
-/*  The Clipboard Project - Cut, copy, and paste anything, anywhere, all from the terminal.
+/*  The Clipboard Project - Cut, copy, and paste anything, anytime, anywhere, all from the terminal.
     Copyright (C) 2023 Jackson Huff and other contributors on GitHub.com
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
