96ecc5ddadcf57a288a234177eb14607ec99e128
Jackson Huff <lightingdzeyenr@gmail.com>
2023-07-14 23:01:42 -0400

    make color formatting 3x faster

--- src/cb/src/utils.cpp
+++ src/cb/src/utils.cpp
@@ -161,31 +161,43 @@ unsigned long levenshteinDistance(const std::string_view& one, const std::string
     return matrix.at(one.size()).at(two.size());
 };
 
-std::string formatColors(const std::string_view& str, bool colorful) {
-    std::string temp(str);
-    for (size_t i = 0; i < temp.size(); i++) {
-        if (temp[i] != '[') continue;
+std::string formatColors(const std::string_view& oldStr, bool colorful) {
+    std::string newStr;
+    newStr.reserve(oldStr.size());
+    for (size_t i = 0, lastAddedi = 0; i < oldStr.size(); i++) {
+        while (oldStr[i] != '[' && i < oldStr.size())
+            i++;
 
-        auto j = temp.find(']', i + 1);
-        if (j == std::string::npos) break;
+        newStr += oldStr.substr(lastAddedi, i - lastAddedi);
+
+        auto j = oldStr.find(']', i + 1);
+        if (j == std::string::npos) {
+            newStr += oldStr[i]; // no match, so just add the bracket
+            break;
+        }
 
         auto matches = [&](const std::string_view& key) {
-            if (key.size() != j - i + 1) return false;
-            for (size_t k = 1; k < key.size() - 1; k++) { // only compare the middle part
-                if (key[k] != temp[i + k]) return false;
-            }
+            for (size_t k = 1; k < key.size() - 1; k++) // only compare the middle part
+                if (key[k] != oldStr[i + k]) return false;
             return true;
         };
 
+        bool matched = false;
+
         for (const auto& key : colors) {
             if (matches(key.first)) {
-                temp.replace(i, key.first.length(), colorful ? key.second : "");
-                if (!colorful) i--; // because i may be at the start of the next color
+                if (colorful) newStr += key.second;
+                i += key.first.length() - 1;
+                matched = true;
                 break;
             }
         }
+
+        if (!matched) newStr += oldStr[i]; // no match, so just add the bracket
+
+        lastAddedi = i + 1;
     }
-    return temp;
+    return newStr;
 }
 
 std::string JSONescape(const std::string_view& input) {
