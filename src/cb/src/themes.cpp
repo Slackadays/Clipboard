ee7fe46511e1135420307619fb9d710b4d1e5434
Slackadays <lightingdzeyenr@gmail.com>
2024-10-14 09:06:59 -0400

    Fix FreeBSD

--- src/cb/src/themes.cpp
+++ src/cb/src/themes.cpp
@@ -14,6 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
 #include "clipboard.hpp"
+#include <unordered_map>
 
 void setTheme(const std::string_view& theme) {
     if (theme == "light") {
